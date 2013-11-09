06a36f3aaf376dc4d98c05ef12edfed6fa741b8b
clemfeelsgood <clementperrot@berkeley.edu>
2013-11-08 22:50:13 -0800

    annotate et static pages

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: flights
+#
+#  id               :integer          not null, primary key
+#  airline          :string(255)
+#  departuretime    :datetime
+#  flightnumber     :string(255)
+#  departureairport :string(255)
+#  arrivalairport   :string(255)
+#  created_at       :datetime
+#  updated_at       :datetime
+#
+
 class Flight < ActiveRecord::Base
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  phone      :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class User < ActiveRecord::Base
 end
