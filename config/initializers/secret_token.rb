2f7ba5bf63d26b63585e9522ef32a6439ff41e1c
clemfeelsgood <clementperrot@berkeley.edu>
2013-11-08 20:00:12 -0800

    back on track

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LaunchHack::Application.config.secret_key_base = 'cb3fc809f7e6b919e0b8be48d5821a3206d4c3150ab891cecc6d27dcc81fa800d94ff1b6553b189a82c91453350b5b09e845fe644c123cf9e828935c8e9bf5c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LaunchHack::Application.config.session_store :cookie_store, key: '_LaunchHack_session'
