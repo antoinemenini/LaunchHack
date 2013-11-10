69e1546dc0e96597e85a385c2b064d13a2901d56
clemfeelsgood <clementperrot@berkeley.edu>
2013-11-09 17:30:41 -0800

    mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,6 +62,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def contact
+    @user_contacted = params[:user_contacted]
+  end
+
+  def send_message
+    @user_contacted = User.find(params[:id])
+    UserMailer.send_message(current_user, @user_contacted, params[:message]).deliver
+    flash[:success] = "Your message has been sent to #{@user_contacted}."
+    redirect_to root_url()
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
